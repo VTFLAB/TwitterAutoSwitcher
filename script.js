439177fb27bee47cdd87c4a26945d51c909d309f
VTF <vtf@msjp.pro>
2023-01-20 12:44:55 +0900

    fix: 何度か切り替わらない場合に遭遇したのでもう少しマシにする。 根本的な原因は https://twitter.com/ にアクセスした場合redirectでhomeに行くと拡張が走らないのが原因
    その他変更
    要素確認を存在だけでなくスタイルのポインタも確認
    await使えるっぽいのでPromiseで処理に変更
    
    タブ切り替え後にも何かしたい場合を想定して個別ファンクションに

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 window.addEventListener("load", main, false);
 
-function main() {
+async function main() {
   // Twitterのフォロー中タブ (2023/01/20時点)
   const tabSelectName =
     "#react-root > div > div > div.css-1dbjc4n.r-18u37iz.r-13qz1uu.r-417010 > main > div > div > div > div > div > div.css-1dbjc4n.r-aqfbo4.r-gtdqiz.r-1gn8etr.r-1g40b8q > div.css-1dbjc4n.r-1e5uvyk.r-5zmot > div:nth-child(2) > nav > div > div.css-1dbjc4n.r-1adg3ll.r-16y2uox.r-1wbh5a2.r-1pi2tsx.r-1udh08x > div > div:nth-child(2) > a";
+  // 対象がアクティブになるまで待機してから切り替え実行
+  await waitForElement(tabSelectName);
+}
 
-  // タイマーイベントで要素が読み込まれるまで実行を続ける
-  const jsInitCheckTimer = setInterval(jsLoaded, 1000);
-  function jsLoaded() {
-    const target = document.querySelector(tabSelectName);
-    // 対象が無い場合は何もしない
-    if (target == null) return;
-
-    //対象が見つかったらタイマー処理解除
-    clearInterval(jsInitCheckTimer);
-    // タブ切り替えする
-    target.click();
-  }
+async function waitForElement(tabSelectName) {
+  await new Promise((resolve) => {
+    const interval = setInterval(() => {
+      // 対象(フォロー中タブ aタグ)
+      const target = document.querySelector(tabSelectName);
+      // 対象のスタイル
+      const elementStyle = window.getComputedStyle(target);
+      // 対象が存在していて、かつポインタイベントがnone以外の場合
+      if (target && elementStyle.pointerEvents !== "none") {
+        // 解除
+        clearInterval(interval);
+        // タブ切り替え
+        target.click();
+        resolve();
+      }
+    }, 1000);
+  });
 }
